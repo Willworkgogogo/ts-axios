f903739b017e25deaf25c205f7749b2707ef5ffd
Willworkgogogo <will_workhard@163.com>
2019-05-06 15:16:37 +0800

    feat(init axios and xhr):

---
+++ src/xhr.ts
@@ -0,0 +1,10 @@
+import { AxiosRequestConfig } from './types'
+
+function xhr(config: AxiosRequestConfig): void {
+	const { url, data = null, method = 'get' } = config
+	const request = new XMLHttpRequest()
+	request.open(method, url, true)
+	request.send(data)
+}
+
+export default xhr
